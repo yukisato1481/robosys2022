d555de25072ca33733df6bf8c97b1bed8d824eae
sato yuki <yukkii1481w@gmail.com>
2022-10-27 12:17:50 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Yuki Sato s21C1056FY@s.chibakoudai.jp
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+       echo NG at Line $1
+       res=1
+}
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng ${LINENO}
 
-[ "${out}" = 14.0 ]
+[ "$res" = 0 ] && echo OK
+exit $res
