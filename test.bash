447f0383493072e18eb346abe3ca5d0f59d464eb
sato yuki <yukkii1481w@gmail.com>
2022-10-27 12:43:08 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Yuki Sato s21C1056FY@s.chibakoudai.jp
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -13,5 +13,14 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15.0 ] || ng ${LINENO}
 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]        || ng ${LINENO}
+[ "${out}" = "" ]   || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]        || ng ${LINENO}
+[ "${out}" = "" ]   || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
-exit $res
+  exit $res
